5124bdb28bd2ce91ff3366d6e2983b95423eb1ee
Ericka Ward <ericka.ward@gmail.com>
2013-06-10 15:27:17 -0400

    first commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ellajune::Application.config.secret_token = '779cb35787ca0dc3f9f185ffc52133833bdedfacee8bc9c2d9f2c44c1d715df62e475a427c84740d45b361228732c4b1029259b1f3e13e499d40515402edf6f9'
+Ellajune::Application.config.secret_token = '8cd2612fc740454986d16bc1f80c99db4193beb487c7211f8245d244727c00c2608fd292d64c3869ed58ae898e99644bcf6f7db1ed99a104aa6236874d7af38f'
