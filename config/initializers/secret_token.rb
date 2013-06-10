ce35ba46e76aa9f7c64c0d3be980acfc6aa8138e
Ericka Ward <ericka.ward@gmail.com>
2013-06-10 15:06:38 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ellajune::Application.config.secret_token = '779cb35787ca0dc3f9f185ffc52133833bdedfacee8bc9c2d9f2c44c1d715df62e475a427c84740d45b361228732c4b1029259b1f3e13e499d40515402edf6f9'
